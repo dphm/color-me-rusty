d7d8a6061078a6d7e22e9185b6f4b4d2335a24a3
quelledanielle <quelledanielle@users.noreply.github.com>
2018-04-16 14:38:03 -0400

    Use constants in Color tests

--- src/color.rs
+++ src/color.rs
@@ -39,87 +39,74 @@ impl Color {
 mod tests {
     use super::*;
 
+    const BLACK: Color = Color(0, 0, 0);
+    const WHITE: Color = Color(255, 255, 255);
+    const RED: Color = Color(255, 0, 0);
+    const BLUE: Color = Color(0, 0, 255);
+
     #[test]
     fn zero_steps() {
-        let black = Color(0, 0, 0);
-        let white = Color(255, 255, 255);
-        assert_eq!(black, Color::step(&black, &white, 0, 0));
+        assert_eq!(BLACK, Color::step(&BLACK, &WHITE, 0, 0));
     }
 
     #[test]
     fn one_step() {
-        let black = Color(0, 0, 0);
-        let white = Color(255, 255, 255);
-        assert_eq!(black, Color::step(&black, &white, 0, 1));
-        assert_eq!(white, Color::step(&black, &white, 1, 1));
+        assert_eq!(BLACK, Color::step(&BLACK, &WHITE, 0, 1));
+        assert_eq!(WHITE, Color::step(&BLACK, &WHITE, 1, 1));
     }
 
     #[test]
     fn two_steps() {
-        let black = Color(0, 0, 0);
         let half = Color(127, 127, 127);
-        let white = Color(255, 255, 255);
-        assert_eq!(black, Color::step(&black, &white, 0, 2));
-        assert_eq!(half, Color::step(&black, &white, 1, 2));
-        assert_eq!(white, Color::step(&black, &white, 2, 2));
+        assert_eq!(BLACK, Color::step(&BLACK, &WHITE, 0, 2));
+        assert_eq!(half, Color::step(&BLACK, &WHITE, 1, 2));
+        assert_eq!(WHITE, Color::step(&BLACK, &WHITE, 2, 2));
     }
 
     #[test]
     fn three_steps() {
-        let black = Color(0, 0, 0);
         let one_third = Color(85, 85, 85);
         let two_thirds = Color(170, 170, 170);
-        let white = Color(255, 255, 255);
-        assert_eq!(black, Color::step(&black, &white, 0, 3));
-        assert_eq!(one_third, Color::step(&black, &white, 1, 3));
-        assert_eq!(two_thirds, Color::step(&black, &white, 2, 3));
-        assert_eq!(white, Color::step(&black, &white, 3, 3));
+        assert_eq!(BLACK, Color::step(&BLACK, &WHITE, 0, 3));
+        assert_eq!(one_third, Color::step(&BLACK, &WHITE, 1, 3));
+        assert_eq!(two_thirds, Color::step(&BLACK, &WHITE, 2, 3));
+        assert_eq!(WHITE, Color::step(&BLACK, &WHITE, 3, 3));
     }
 
     #[test]
     fn zero_steps_down() {
-        let black = Color(0, 0, 0);
-        let white = Color(255, 255, 255);
-        assert_eq!(white, Color::step(&white, &black, 0, 0));
+        assert_eq!(WHITE, Color::step(&WHITE, &BLACK, 0, 0));
     }
 
     #[test]
     fn one_step_down() {
-        let black = Color(0, 0, 0);
-        let white = Color(255, 255, 255);
-        assert_eq!(white, Color::step(&white, &black, 0, 1));
-        assert_eq!(black, Color::step(&white, &black, 1, 1));
+        assert_eq!(WHITE, Color::step(&WHITE, &BLACK, 0, 1));
+        assert_eq!(BLACK, Color::step(&WHITE, &BLACK, 1, 1));
     }
 
     #[test]
     fn two_steps_down() {
-        let black = Color(0, 0, 0);
         let half = Color(127, 127, 127);
-        let white = Color(255, 255, 255);
-        assert_eq!(white, Color::step(&white, &black, 0, 2));
-        assert_eq!(half, Color::step(&white, &black, 1, 2));
-        assert_eq!(black, Color::step(&white, &black, 2, 2));
+        assert_eq!(WHITE, Color::step(&WHITE, &BLACK, 0, 2));
+        assert_eq!(half, Color::step(&WHITE, &BLACK, 1, 2));
+        assert_eq!(BLACK, Color::step(&WHITE, &BLACK, 2, 2));
     }
 
     #[test]
     fn three_steps_down() {
-        let black = Color(0, 0, 0);
         let one_third = Color(85, 85, 85);
         let two_thirds = Color(170, 170, 170);
-        let white = Color(255, 255, 255);
-        assert_eq!(white, Color::step(&white, &black, 0, 3));
-        assert_eq!(two_thirds, Color::step(&white, &black, 1, 3));
-        assert_eq!(one_third, Color::step(&white, &black, 2, 3));
-        assert_eq!(black, Color::step(&white, &black, 3, 3));
+        assert_eq!(WHITE, Color::step(&WHITE, &BLACK, 0, 3));
+        assert_eq!(two_thirds, Color::step(&WHITE, &BLACK, 1, 3));
+        assert_eq!(one_third, Color::step(&WHITE, &BLACK, 2, 3));
+        assert_eq!(BLACK, Color::step(&WHITE, &BLACK, 3, 3));
     }
 
     #[test]
     fn two_steps_unequal_values() {
-        let red = Color(255, 0, 0);
         let mid = Color(127, 0, 127);
-        let blue = Color(0, 0, 255);
-        assert_eq!(red, Color::step(&red, &blue, 0, 2));
-        assert_eq!(mid, Color::step(&red, &blue, 1, 2));
-        assert_eq!(blue, Color::step(&red, &blue, 2, 2));
+        assert_eq!(RED, Color::step(&RED, &BLUE, 0, 2));
+        assert_eq!(mid, Color::step(&RED, &BLUE, 1, 2));
+        assert_eq!(BLUE, Color::step(&RED, &BLUE, 2, 2));
     }
 }
