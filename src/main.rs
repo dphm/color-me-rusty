818d0b49247e2b475f36ce07cae315bfdca2af62
quelledanielle <quelledanielle@users.noreply.github.com>
2018-04-16 15:40:18 -0400

    Minor updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![feature(range_contains)]
-
 mod color;
 
 use color::Color;
@@ -7,7 +5,6 @@ use color::Color;
 const RED: Color = Color(255, 0, 0);
 const GREEN: Color = Color(0, 255, 0);
 const BLUE: Color = Color(0, 0, 255);
-const NUM_FRAMES: u32 = 200;
 
 extern {
     fn jsSetBackgroundColor(valsPtr: *const u8);
@@ -16,7 +13,7 @@ extern {
 #[no_mangle]
 pub extern fn draw_frame(n: u32) {
     let colors = vec![&RED, &GREEN, &BLUE];
-    let color = Color::interpolate_linear(&colors, n, NUM_FRAMES);
+    let color = Color::interpolate_linear(&colors, n, 200);
     let vals = vec![color.0, color.1, color.2];
     unsafe { jsSetBackgroundColor(vals.as_ptr()); }
 }
